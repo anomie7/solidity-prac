f75e0ce679b67f4b8764feac48d2baef4e998c82
Justin <anomie7777@gmail.com>
2019-01-09 15:34:32 +0900

    [impl] 4-4 회원 관리

--- chapter4/OreOreCoin.sol
+++ chapter4/OreOreCoin.sol
@@ -1,29 +1,111 @@
 pragma solidity ^0.5.0;
 
-contract OreOreCoin {
+contract Owned {
+    address public owner;
+
+    constructor () public {
+        owner = msg.sender;
+    }
+
+    event TransferOwnership(address oldaddr, address newaddr);
+
+    modifier onlyOwner(){ 
+        if(msg.sender != owner){
+            revert("this function can invoke owner");
+        }
+        _;
+    }
+
+    function transferOwnership(address _new) public onlyOwner{
+        address oldOwner = owner;
+        owner = _new;
+        emit TransferOwnership(oldOwner, owner);
+    }
+}
+
+contract Members is Owned {
+    address public coin;
+    MemberStatus[] public status;
+    mapping(address => History) public tradingHistory;
+
+    struct MemberStatus {
+        string name;
+        uint256 times;
+        uint256 sum;
+        int8 rate;
+    }
+
+    struct History {
+        uint256 times;
+        uint256 sum;
+        uint256 statusIndex;
+    }
+
+    modifier onlyCoin() { 
+        if(msg.sender == coin){
+            _;
+        } 
+    }
+
+    function setCoin(address _addr) public onlyOwner {
+        coin = _addr;
+    }
+
+    function pushStatus(string memory _name, uint256 _times, uint256 _sum, int8 _rate) public onlyOwner {
+        status.push(MemberStatus({
+            name: _name,
+            times: _times,
+            sum: _sum,
+            rate: _rate
+        }));
+    }
+
+    function editStatus(uint _index, string memory _name, uint256 _times, uint256 _sum, int8 _rate) public onlyOwner {
+        if(_index < status.length){
+            status[_index].name = _name;
+            status[_index].times = _times;
+            status[_index].sum = _sum;
+            status[_index].rate = _rate;
+        }
+    }
+
+    function updateHistory(address _member, uint256 _value) public onlyCoin {
+        tradingHistory[_member].times += 1;
+        tradingHistory[_member].sum += _value;
+        uint256 index;
+        int8 tmprate;
+
+        for (uint i = 0; i < status.length; i++) {
+            if(tradingHistory[_member].times >= status[i].times &&
+            tradingHistory[_member].sum >= status[i].sum &&
+               tmprate < status[i].rate ){
+                index = i;
+               }
+        }
+
+        tradingHistory[_member].statusIndex = index;
+    }
+
+    function getCashbackRate(address _member) public view returns (int8 rate) {
+        uint256 statusIndex = tradingHistory[_member].statusIndex;
+        rate = status[statusIndex].rate;
+    }
+}
+
+contract OreOreCoin is Owned {
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
     mapping (address => uint256) public balanceOf;
     mapping (address => int8) blackList;
-    mapping (address => int8) cashbackRate;
-    address public owner;
-
-    modifier onlyOwner() { 
-        require(msg.sender == owner, "this function can invoke contract owner!"); 
-        // "_;" 를 깜빡하지 마세요! 수정자가
-        // 사용 될 때, "_;"가 실제 함수
-        // 본문으로 대체됩니다.
-        _; 
-    }
+    mapping (address => Members) public members;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Blacklisted(address indexed target);
     event DeleteFromBlacklist(address indexed target);
     event RejectedPaymentToBlacklistedAddr(address indexed from, address indexed to, uint256 value);
     event RejectedPaymentFromBlacklistAddr(address indexed from, address indexed to, uint256 value);
-    event SetCashback(address indexed addr, int8 rate);
     event Cashback(address indexed from, address indexed to, uint256 value);
 
     constructor (uint256 _supply, string memory _name, string memory _symbol, uint8 _decimals) public{
@@ -32,7 +114,6 @@ contract OreOreCoin {
         symbol = _symbol;
         decimals = _decimals;
         totalSupply = _supply;
-        owner = msg.sender;
     }
 
     function blacklisting(address _addr) public onlyOwner {
@@ -45,35 +126,25 @@ contract OreOreCoin {
         emit DeleteFromBlacklist(_addr);
     }
 
-    function setCashbackRate(int8 _rate) public {
-        if (_rate < 1) {
-            _rate = -1;
-        } else if (_rate > 100) {
-            _rate = 100;
-        }
-
-        cashbackRate[msg.sender] = _rate;
-
-        if(_rate < 1 ) {
-            _rate = 0;
-        }
-
-        emit SetCashback(msg.sender, _rate);
+    function setMembers(Members _members) public {
+        members[msg.sender] = Members(_members);
     }
 
     function transfer(address _to, uint256 _value) public payable{
-        if (balanceOf[msg.sender] < _value) revert("value invalid");
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert("value invalid");
+        if (balanceOf[msg.sender] < _value) revert("invalid value");
+        if (balanceOf[_to] + _value < balanceOf[_to]) revert("invalid value");
 
         if (blackList[msg.sender] > 0) {
             emit RejectedPaymentFromBlacklistAddr(msg.sender, _to, _value);
         } else if (blackList[_to] > 0){
             emit RejectedPaymentToBlacklistedAddr(msg.sender, _to, _value);
         } else {
-
             uint256 cashback = 0;
 
-            if(cashbackRate[_to] > 0) cashback = _value / 100 * uint256(cashbackRate[_to]);
+            if(address(members[_to]) > address(0)) {
+                cashback = _value / 100 * uint256(members[_to].getCashbackRate(msg.sender));
+                members[_to].updateHistory(msg.sender, _value);
+            }
 
             balanceOf[msg.sender] -= (_value - cashback);
             balanceOf[_to] += (_value - cashback);
